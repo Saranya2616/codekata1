55a19b2aa528643d8f12f1b0e9f6bffd9f998d01
Saranya2616 <52150819+Saranya2616@users.noreply.github.com>
2019-07-09 10:38:18 +0530

    Add files via upload

---
+++ sample3.py
@@ -0,0 +1,8 @@
+b=str(input());
+if ((b>='a') & (b<='z')or (b>='A') & (b<='Z')): 
+    if b=='a'or b=='e'or b=='i'or b=='o'or b=='u'or b=='A'or b=='E'or b=='I'or b=='O'or b=='U':
+        print("vowel")
+    else:
+        print("consonant")
+else:
+    print("invalid")
