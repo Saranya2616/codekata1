d8be539a12616f2b43b6372272be5387b64314dd
Saranya2616 <52150819+Saranya2616@users.noreply.github.com>
2019-07-09 13:11:09 +0530

    Update sample5.py

--- sample5.py
+++ sample5.py
@@ -1,9 +1,9 @@
-a=int(input());
-b=int(input());
-c=int(input());
-if (a>=b)&(a>=c):
-    print(a)
-elif (b>=a)&(b>=c):
-    print(b)
+n1=int(input());
+n2=int(input());
+n3=int(input());
+if (n1>=n2)&(n1>=n3):
+    print(n1)
+elif (n2>=n1)&(n2>=n3):
+    print(n2)
 else:
-    print(c)
+    print(n3)
