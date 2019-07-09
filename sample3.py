f6d2178d6333b9d3853db4e6569ee959efe230ac
Saranya2616 <52150819+Saranya2616@users.noreply.github.com>
2019-07-09 10:47:41 +0530

    Update sample3.py

--- sample3.py
+++ sample3.py
@@ -3,6 +3,6 @@ if ((b>='a') & (b<='z')or (b>='A') & (b<='Z')):
     if b=='a'or b=='e'or b=='i'or b=='o'or b=='u'or b=='A'or b=='E'or b=='I'or b=='O'or b=='U':
         print("vowel")
     else:
-        print("consonant")
+        print("Consonant")
 else:
     print("invalid")
