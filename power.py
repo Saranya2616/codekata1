a4860ad927cbcc0b8274b4d468aa1cef71085b4e
Saranya Ganesh <52150819+Saranya2616@users.noreply.github.com>
2019-08-05 17:27:24 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,10 @@
+num1=int(input())
+num2=int(input())
+if num1 & num2 is int:
+    num3=num1**num2
+    print(num3)
+else:
+    num3=int(num1)
+    num4=int(num2)
+    num5=num3**num4
+    print(num5)
