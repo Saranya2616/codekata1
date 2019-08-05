443ee5f548335db755ec1d6fda16c0df3a3f3b4e
Saranya Ganesh <52150819+Saranya2616@users.noreply.github.com>
2019-08-05 18:34:59 +0530

    Update power.py

--- power.py
+++ power.py
@@ -4,7 +4,8 @@ if num2 & num3 is int:
     num4=num2**num3
     print(num4)
 else:
-    [num5,num6]=list(map(int,input().split()))
+    num5=int(num2)
+    num6=int(num3)
     
     num7=num5**num6
     print(num7)
